f0de49cd2f6a758cf57319324832cd6bb7c1f0da
mharizanova8703 <mharizanova@yahoo.com>
2021-09-13 15:13:47 -0400

    Creating  variables
    adding a password length   between  8 and 128 characters

--- script.js
+++ script.js
@@ -3,6 +3,22 @@ var generateBtn = document.querySelector('#generate')
 
 // Write password to the #password input
 function writePassword() {
+  var lowerCase = 'abcdefghijklmnopqrstuvwxyz'
+  var upperCase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+  var numbers = '0123456789'
+  var specialChar = '!@#$%^&*()_+{}:'
+  ;('[]<>?/.,')
+
+  var userChoices = ''
+
+  var passwordLength = parseInt(
+    window.prompt(
+      'Please choose the amount of characters you would like in your password between 8 and 128',
+    ),
+  )
+
+  console.log('password length', passwordLength)
+
   var password = generatePassword()
   var passwordText = document.querySelector('#password')
 
