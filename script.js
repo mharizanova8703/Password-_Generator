671d46809ce37b23284e52c2cde3cef94ed6c979
mharizanova8703 <mharizanova@yahoo.com>
2021-09-14 22:01:56 -0400

    updating js

--- script.js
+++ script.js
@@ -10,61 +10,65 @@ function writePassword() {
   ;('[]<>?/.,')
 
   var userChoices = ''
-
-  var passwordLength = parseInt(
-    window.prompt(
+  while (true) {
+    var passwordLength = window.prompt(
       'Please choose the amount of characters you would like in your password between 8 and 128',
-    ),
-  )
-
-  console.log('password length', passwordLength)
-
-  if (passwordLength < 8 || passwordLength > 128) {
-    alert('Please choose a number between 8 and 128')
-    return
-  } else {
-    var littleLetter = window.confirm(
-      'Would you like lowercase letters in your password?',
     )
-    console.log('little numbers confirm', littleLetter)
 
-    var bigLetter = window.confirm(
-      'Would you like uppercase letters in your password?',
-    )
-    var num = window.confirm('Would you like numbers in your password?')
+    console.log('password length', passwordLength)
 
-    var specialCharacterConfirm = window.confirm(
-      'Would you like special characters in your password?',
-    )
-    if (littleLetter === true) {
-      userChoices += lowerCase
+    if (
+      passwordLength < 8 ||
+      passwordLength > 128 ||
+      passwordLength == parseInt(null)
+    ) {
+      alert('Please choose a number between 8 and 128')
+      //when is false is breaking the loop
+    } else {
+      break
     }
-
-    if (bigLetter === true) {
-      userChoices += upperCase
-    }
-
-    if (num === true) {
-      userChoices += numbers
-    }
-
-    if (specialCharacterConfirm === true) {
-      userChoices += specialChar
-    }
-
-    console.log('user choices', userChoices)
-    var characters = ''
-    for (var i = 0; i < passwordLength; i++) {
-      var password = Math.floor(userChoices.length * Math.random())
-
-      characters += userChoices.charAt(password)
-      console.log('password', password)
-    }
-
-    var passwordText = document.querySelector('#password')
-
-    passwordText.value = characters
   }
+  var littleLetter = window.confirm(
+    'Would you like lowercase letters in your password?',
+  )
+  console.log('little numbers confirm', littleLetter)
+
+  var bigLetter = window.confirm(
+    'Would you like uppercase letters in your password?',
+  )
+  var num = window.confirm('Would you like numbers in your password?')
+
+  var specialCharacterConfirm = window.confirm(
+    'Would you like special characters in your password?',
+  )
+  if (littleLetter === true) {
+    userChoices += lowerCase
+  }
+
+  if (bigLetter === true) {
+    userChoices += upperCase
+  }
+
+  if (num === true) {
+    userChoices += numbers
+  }
+
+  if (specialCharacterConfirm === true) {
+    userChoices += specialChar
+  }
+
+  console.log('user choices', userChoices)
+  var characters = ''
+  for (var i = 0; i < passwordLength; i++) {
+    var password = Math.floor(userChoices.length * Math.random())
+
+    characters += userChoices.charAt(password)
+    console.log('password', password)
+  }
+
+  var passwordText = document.querySelector('#password')
+
+  passwordText.value = characters
 }
 
 // Add event listener to generate button
